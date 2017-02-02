97fef306c89757f17a1af049033e085f62c6d63d
reka <rekadowney@163.com>
2017-02-02 10:45:29 +0800

    项目初始化

---
+++ src/main/java/me/junbin/gradprj/domain/BaseDomain.java
@@ -0,0 +1,11 @@
+package me.junbin.gradprj.domain;
+
+/**
+ * @author : Zhong Junbin
+ * @email : <a href="mailto:rekadowney@163.com">发送邮件</a>
+ * @createDate : 2017/2/2 10:45
+ * @description :
+ */
+public class BaseDomain {
+
+}
